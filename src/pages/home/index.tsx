301deadff996bf513f667176f78e173a824cfa01
hoaxvo16 <dev.h1620@gmail.com>
2021-11-07 12:11:09 +0700

    add facebook login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react';
 import { useState } from 'react';
-import { googleProvider } from 'firebase-services/provider';
+import { googleProvider, facebookProvider } from 'firebase-services/provider';
 import { socialLogin } from 'firebase-services/auth';
 import { HomeViewModel } from './home-view-models';
 
@@ -15,10 +15,15 @@ export const HomePage = observer(() => {
     socialLogin(googleProvider);
   };
 
+  const loginWithFacebook = () => {
+    socialLogin(facebookProvider);
+  };
+
   return (
     <div>
       <p>{viewModal.todo.name}</p>
       <button onClick={loginWithGoogle}>Login with google</button>
+      <button onClick={loginWithFacebook}>Login with facebook</button>
       <button onClick={handleClick}>Get random to do</button>
     </div>
   );
