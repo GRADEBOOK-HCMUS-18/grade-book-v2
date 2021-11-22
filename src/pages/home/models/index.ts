15b9520e86781bb68b62c9bec6662f05b98bfe4a
hoaxvo16 <dev.h1620@gmail.com>
2021-11-22 20:04:54 +0700

    remove main teacher class

--- src/pages/home/models/index.ts
+++ src/pages/home/models/index.ts
@@ -1,25 +1,17 @@
+import { User } from 'shared/models';
+
 export class SingleClass {
-    id:number= 0
-    roleOfCurrentUser:string=""
-    name:string=""
-    room:string = ""
-    startDate:Date= new Date()
-    description:string = ''
-    mainTeacher: MainTeacher  = new MainTeacher()
-    inviteStringStudent:string = ''
-    InviteStringTeacher:string = ''
+  id: number = 0;
+  roleOfCurrentUser: string = '';
+  name: string = '';
+  room: string = '';
+  startDate: Date = new Date();
+  description: string = '';
+  mainTeacher: User = new User();
+  inviteStringStudent: string = '';
+  InviteStringTeacher: string = '';
 }
 
-export class MainTeacher{
-email:string = ""
-firstName:string= ""
-lastName:string =""  
-profilePictureUrl:string = ''
-defaultProfilePictureHex:string  = '' 
-displayName?:string = ''
-
-}
-
-export class ClassListResponse{
-    classList:SingleClass[] = []
+export class ClassListResponse {
+  classList: SingleClass[] = [];
 }
