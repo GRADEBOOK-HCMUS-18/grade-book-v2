eb6d0df08e07272d177f532db86b7a345ec29b1e
truongtrieu123 <62706706+truongtrieu123@users.noreply.github.com>
2021-11-22 17:23:29 +0700

    fix responsive and route

--- src/pages/home/components/dashboard/components/class-card.tsx
+++ src/pages/home/components/dashboard/components/class-card.tsx
@@ -23,86 +23,91 @@ export const ClassCard = observer(
     };
 
     return (
-      <Col className="mb-3">
-        <Card className="class-card border-1 p-0">
-          <Card.Header className="class-header px-0 py-3">
-            <Card.Title className="d-flex">
-              <OverlayTrigger
-                trigger="focus"
-                placement="bottom-end"
-                overlay={
-                  <Popover>
-                    <Popover.Body className="p-0">
-                      <div className="list-group">
-                        {roleOfCurrentUser === 'student' ? (
+      <Card className="class-card border-1 p-0 mx-2 my-3">
+        <Card.Header className="class-header px-0 py-3">
+          <Card.Title className="d-flex">
+            <OverlayTrigger
+              trigger="focus"
+              placement="bottom-end"
+              overlay={
+                <Popover>
+                  <Popover.Body className="p-0">
+                    <div className="list-group">
+                      {roleOfCurrentUser === 'student' ? (
+                        <button
+                          type="button"
+                          className="list-group-item list-group-item-action"
+                        >
+                          Hủy đăng ký
+                        </button>
+                      ) : (
+                        <div>
+                          <button
+                            type="button"
+                            className="list-group-item list-group-item-action"
+                            onClick={() => openInvitationModal()}
+                          >
+                            Link mời vào lớp
+                          </button>
                           <button
                             type="button"
                             className="list-group-item list-group-item-action"
                           >
-                            Hủy đăng ký
+                            Lưu trữ
                           </button>
-                        ) : (
-                          <div>
-                            <button
-                              type="button"
-                              className="list-group-item list-group-item-action"
-                              onClick={() => openInvitationModal()}
-                            >
-                              Link mời vào lớp
-                            </button>
-                            <button
-                              type="button"
-                              className="list-group-item list-group-item-action"
-                            >
-                              Lưu trữ
-                            </button>
-                          </div>
-                        )}
-                      </div>
-                    </Popover.Body>
-                  </Popover>
+                        </div>
+                      )}
+                    </div>
+                  </Popover.Body>
+                </Popover>
+              }
+            >
+              <button className="btn-three-dot">
+                <BsThreeDotsVertical></BsThreeDotsVertical>
+              </button>
+            </OverlayTrigger>
+            <div className="ps-3">
+              <a
+                className="go-to-class d-block "
+                href={
+                  roleOfCurrentUser === 'student'
+                    ? `/class/${id}`
+                    : `/t/class/${id}`
                 }
               >
-                <button className="btn-three-dot">
-                  <BsThreeDotsVertical></BsThreeDotsVertical>
-                </button>
-              </OverlayTrigger>
-              <div className="ps-3">
-                <a className="go-to-class d-block " href="#/go-to-class">
-                  <div className="class-title text-white text-truncate">
-                    {name}
-                  </div>
-                  <div className="class-room  text-white text-truncate">
-                    {room}
-                  </div>
-                </a>
-                <span className="class-teacher-name  text-white text-truncate">
-                  {mainTeacher.displayName}
-                </span>
-              </div>
-            </Card.Title>
-          </Card.Header>
-          <Card.Body className="card-body" />
-          <Card.Footer className="card-footer bg-white d-flex flex-row-reverse">
-            <OverlayTrigger
-              placement="bottom-end"
-              overlay={<Tooltip>Mở folder cho {name}</Tooltip>}
-            >
-              <button className="btn-floating">
-                <AiOutlineFolderOpen />
-              </button>
-            </OverlayTrigger>
-            <OverlayTrigger
-              placement="bottom-end"
-              overlay={<Tooltip>Mở bài tập cho {name}</Tooltip>}
-            >
-              <button className="btn-floating">
-                <MdAssignmentInd />
-              </button>
-            </OverlayTrigger>
-          </Card.Footer>
-        </Card>
-      </Col>
+                <div className="class-title text-white text-truncate">
+                  {name}
+                </div>
+                <div className="class-room  text-white text-truncate">
+                  {room ? room : <br />}
+                </div>
+              </a>
+              <span className="class-teacher-name  text-white text-truncate">
+                {mainTeacher.displayName}
+              </span>
+            </div>
+          </Card.Title>
+        </Card.Header>
+        <Card.Body className="card-body" />
+        <Card.Footer className="card-footer bg-white d-flex flex-row-reverse">
+          <OverlayTrigger
+            placement="bottom-end"
+            overlay={<Tooltip>Mở folder cho {name}</Tooltip>}
+          >
+            <button className="btn-floating">
+              <AiOutlineFolderOpen />
+            </button>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="bottom-end"
+            overlay={<Tooltip>Mở bài tập cho {name}</Tooltip>}
+          >
+            <button className="btn-floating">
+              <MdAssignmentInd />
+            </button>
+          </OverlayTrigger>
+        </Card.Footer>
+      </Card>
     );
   }
 );
