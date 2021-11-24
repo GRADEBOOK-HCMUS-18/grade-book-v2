1b02cc25cba87854000558ae6cc054d1350d545f
hoaxvo16 <dev.h1620@gmail.com>
2021-11-24 16:51:59 +0700

    change behavior of pop profile

--- src/layout/navbar/components/pop-over-profile/index.tsx
+++ src/layout/navbar/components/pop-over-profile/index.tsx
@@ -1,6 +1,5 @@
 import { observer } from 'mobx-react';
-import { useRef, useState } from 'react';
-import { Overlay, Popover } from 'react-bootstrap';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useHistory } from 'react-router-dom';
 import { Avatar } from 'shared/components';
@@ -8,21 +7,9 @@ import { userViewModel } from 'shared/view-models';
 import './style/index.css';
 
 export const PopOverProfile = observer(() => {
-  const [show, setShow] = useState(false);
-
-  const [target, setTarget] = useState(null);
-
   const history = useHistory();
 
-  const ref = useRef(null);
-
-  const handleClick = (event: any) => {
-    setShow(!show);
-    setTarget(event.target);
-  };
-
   const gotoProfile = () => {
-    setShow(!show);
     history.push('/profile');
   };
 
@@ -33,10 +20,11 @@ export const PopOverProfile = observer(() => {
 
   const user = userViewModel.user;
   return (
-    <div ref={ref}>
-      <Avatar onClick={handleClick} user={user} />
-      <Overlay show={show} target={target} placement="bottom" container={ref}>
-        <Popover id="popover-contained">
+    <OverlayTrigger
+      trigger="focus"
+      placement="bottom"
+      overlay={
+        <Popover id="pop">
           <Popover.Body>
             <div className="center-vertical">
               <Avatar size={80} user={user} />
@@ -61,7 +49,11 @@ export const PopOverProfile = observer(() => {
             </div>
           </Popover.Body>
         </Popover>
-      </Overlay>
-    </div>
+      }
+    >
+      <button className="flex-btn">
+        <Avatar user={user} size={35} />
+      </button>
+    </OverlayTrigger>
   );
 });
