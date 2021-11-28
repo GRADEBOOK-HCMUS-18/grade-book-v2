011726723cb4a82e4c785ce388e5f5a89221463d
truongtrieu123 <62706706+truongtrieu123@users.noreply.github.com>
2021-11-28 22:59:27 +0700

    update class grade api

--- src/pages/class-detail/components/class-dashboard/components/class-grade-management/components/class-grade-view-model.ts
+++ src/pages/class-detail/components/class-dashboard/components/class-grade-management/components/class-grade-view-model.ts
@@ -1,38 +1,15 @@
-import { BaseViewModel } from 'shared/view-models';
 import {observable, action, makeObservable} from 'mobx'
+import { BaseViewModel,lineLoadingViewModel, classDetailViewModel} from 'shared/view-models';
 import {GradeCategory} from 'shared/models'
+import { HttpError } from 'shared/errors';
+import { httpService } from 'shared/services';
 
-
-class ClassGradeViewModel extends BaseViewModel{
+export class ClassGradeViewModel extends BaseViewModel{
     gradeStructureList:GradeCategory[]=[]
     
     constructor()
     {
         super();
-        let item = new GradeCategory()
-        item.id = 1;
-        item.title = 'final term';
-        item.grade = '10';
-        this.gradeStructureList.push(item)
-
-        item = new GradeCategory()
-        item.id = 2;
-        item.title = 'mid term';
-        item.grade = '10';
-        this.gradeStructureList.push(item)
-
-        item = new GradeCategory()
-        item.id = 3;
-        item.title = 'excercise 1';
-        item.grade = '10';
-        this.gradeStructureList.push(item)
-
-        item = new GradeCategory()
-        item.id = 4;
-        item.title = 'excercise 2';
-        item.grade = '10';
-        this.gradeStructureList.push(item)
-
         makeObservable(this,{
             gradeStructureList:observable,
             fetchGradeStructure:action,
@@ -43,44 +20,126 @@ class ClassGradeViewModel extends BaseViewModel{
         })
     }
 
-    fetchGradeStructure(){
+    async fetchGradeStructure(){
+        const {id} = classDetailViewModel.classInfo;
+        lineLoadingViewModel.startLoading();
+
+        const response:GradeCategory[]|HttpError =await httpService.sendGet(`/Class/${id}/assignment`, httpService.getBearerToken())
+        if(response instanceof HttpError){
+            this.makeError('Không thể tải được trang, vui lòng thử lại sau')
+            lineLoadingViewModel.stopLoading();
+            return false;
+        }
+        else{
+            this.gradeStructureList = response;
+            lineLoadingViewModel.stopLoading();
+            return true;
+        }
         
     }
 
-    reorderGradeStructure(startIndex:number, finishIndex:number){
+    async reorderGradeStructure(startIndex:number, finishIndex:number){
+        const {id} = classDetailViewModel.classInfo;
+    
         const newArray = Array.from (this.gradeStructureList);
         const temp:GradeCategory = newArray[startIndex];
+    
         newArray.splice(startIndex,1);
         newArray.splice(finishIndex,0,temp);
-        this.gradeStructureList= newArray;
-    }
-
-    addGradeCategory(value: GradeCategory){
-        const newValue :GradeCategory= {
-            id: this.gradeStructureList.length+100,
-            title: value.title,
-            grade: value.grade
+    
+        const orderArray:number[] = newArray.map(item=>item.id)
+        const body ={
+            assignmentIdPriorityOrder:orderArray,
         }
-        const newArray = Array.from (this.gradeStructureList);
-        newArray.push(newValue);
-        this.gradeStructureList= newArray;
+        const response:GradeCategory[]|HttpError =await  httpService.sendPut(`/class/${id}/assignment/priority`,body, httpService.getBearerToken());
+
+        if(response instanceof HttpError)
+        {
+            this.makeError("Đổi thứ tự không thành công, vui lòng thử lại sau");
+            return false;
+        }
+        else{
+            this.gradeStructureList= newArray;
+            return true;
+        }
+    
     }
 
-    deleteGradeCategory(id:number)
-    {
-        const newList = this.gradeStructureList.filter(item=>item.id!==id)
-        this.gradeStructureList = newList;
+    async addGradeCategory(value: GradeCategory){
+        const {id} = classDetailViewModel.classInfo;
+        lineLoadingViewModel.startLoading();
+        const body:any= {
+            name: value.name,
+            point: value.point
+        }
+        const response: GradeCategory|HttpError = await httpService.sendPost(`/class/${id}/assignment`,body, httpService.getBearerToken());
+        if(response instanceof HttpError)
+        {
+            this.makeError('Không thể tạo bài tập mới, vui lòng thử lại sau')
+            lineLoadingViewModel.stopLoading();
+            return false;
+        }
+        else
+        {
+            const newArray:GradeCategory[] = Array.from(this.gradeStructureList);
+            newArray.push(response);
+            this.gradeStructureList=newArray;
+
+            lineLoadingViewModel.stopLoading();
+            return true;
+        }
     }
 
-    updateGradeCategory(id:number, updateValue :GradeCategory)
+    async deleteGradeCategory(assignmentId:number)
     {
-        console.log(updateValue);
-        const item= this.gradeStructureList.find(item=>item.id===id)
-        if(item){
-            const index = this.gradeStructureList.indexOf(item);
-            item.grade = updateValue.grade;
-            item.title = updateValue.title;
-            this.gradeStructureList.splice(index,1,item);
+        const {id} = classDetailViewModel.classInfo;
+        const temp = this.gradeStructureList.find(item=>item.id ===assignmentId);
+        if(temp){
+            lineLoadingViewModel.startLoading();
+            const index = this.gradeStructureList.indexOf(temp);
+            const response: string|HttpError = await httpService.sendDelete(`/class/${id}/assignment/${assignmentId}`, httpService.getBearerToken());
+            if(response instanceof HttpError){
+                this.makeError('Không thể xóa bài tập, vui lòng thử lại sau')
+                lineLoadingViewModel.stopLoading();
+                return false;
+            }
+            else{
+                const newArray = Array.from(this.gradeStructureList);
+                newArray.splice(index,1);
+                this.gradeStructureList=newArray;
+
+                lineLoadingViewModel.stopLoading();
+                return true;
+            }
+        }
+    }
+
+    async updateGradeCategory(assignmentId:number, updateValue :GradeCategory)
+    {
+        const {id} = classDetailViewModel.classInfo;
+        const temp= this.gradeStructureList.find(item=>item.id===assignmentId)
+        if(temp){
+            lineLoadingViewModel.startLoading();
+            const index = this.gradeStructureList.indexOf(temp);
+            const body:any = {name:updateValue.name, point:updateValue.point}
+            
+            const response: GradeCategory|HttpError = await httpService.sendPut(`/class/${id}/assignment/${assignmentId}`,body, httpService.getBearerToken());
+            
+            if(response instanceof HttpError){
+                this.makeError('Không thể thay đổi nội dung, vui lòng thử lại sau')
+                lineLoadingViewModel.stopLoading();
+            }
+            else{
+                temp.name =updateValue.name;
+                temp.point = updateValue.point;
+
+                const newArray = Array.from(this.gradeStructureList);
+                newArray.splice(index,1,temp);
+                this.gradeStructureList=newArray;
+                
+                lineLoadingViewModel.stopLoading();
+                return false;
+            }
         }
     }
 }
