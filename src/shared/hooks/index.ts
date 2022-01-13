cfd7bbef6fdfe417a926c1a84c3f21ba69d8cff5
truongtrieu123 <uerittruong@gmail.com>
2022-01-13 09:33:25 +0700

    create useCountDownTimer hook

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -52,3 +52,135 @@ export function useFormFields(initialState:any) {
     }
   ];
 }
+
+type countDownActions =
+  | { type: 'START' }
+  | { type: 'RESET'; payload: number }
+  | { type: 'PAUSE' }
+  | { type: 'RUNNING' }
+  | { type: 'TICK'; payload: number }
+
+type countDownState = {
+  canStart: boolean
+  countdown: number
+  isRunning: boolean
+}
+
+function countDownReducer(state: countDownState, action: countDownActions) {
+  switch (action.type) {
+    case 'START':
+      return {
+        ...state,
+        canStart: state.countdown !== 0,
+      }
+    case 'RESET':
+      return {
+        ...state,
+        countdown: action.payload,
+        canStart: false,
+        isRunning: false,
+      }
+    case 'PAUSE':
+      return {
+        ...state,
+        canStart: false,
+        isRunning: false,
+      }
+    case 'RUNNING':
+      return {
+        ...state,
+        isRunning: true,
+      }
+    case 'TICK':
+      return {
+        ...state,
+        countdown: state.countdown - action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export function useCountdownTimer(
+  timer:number=0,
+  interval:number = 1000,
+  autostart:boolean = false,
+  expireImmediate:boolean = false,
+  resetOnExpire:boolean = true,
+  onExpire?:()=>void,
+  onReset?:()=>void,
+): {
+  countdown: number,
+  isRunning: boolean,
+  start: () => void,
+  reset: () => void,
+  pause: () => void} {
+  const [state, dispatch] = useReducer(countDownReducer, {
+    canStart: autostart,
+    countdown: timer,
+    isRunning: false,
+  })
+
+  function start() {
+    dispatch({ type: 'START' })
+  }
+
+  function pause() {
+    dispatch({ type: 'PAUSE' })
+  }
+
+  function initStopped(time: number) {
+    dispatch({ type: 'RESET', payload: time })
+  }
+
+  const reset = useCallback(() => {
+    initStopped(timer)
+    if (onReset && typeof onReset === 'function') {
+      onReset()
+    }
+  }, [timer, onReset])
+
+  const expire = useCallback(() => {
+    initStopped(resetOnExpire ? timer : 0)
+    if (onExpire && typeof onExpire === 'function') {
+      onExpire()
+    }
+  }, [timer, onExpire, resetOnExpire])
+
+  useEffect(() => {
+    function tick() {
+      if (
+        state.countdown / 1000 <= 0 ||
+        (expireImmediate && (state.countdown - interval) / 1000 <= 0)
+      ) {
+        expire()
+      } else {
+        dispatch({ type: 'TICK', payload: interval })
+      }
+    }
+
+    let id: NodeJS.Timeout
+    if (state.canStart) {
+      id = setInterval(tick, interval)
+      if (!state.isRunning) {
+        dispatch({ type: 'RUNNING' })
+      }
+    }
+    return () => clearInterval(id)
+  }, [
+    expire,
+    expireImmediate,
+    interval,
+    state.canStart,
+    state.countdown,
+    state.isRunning,
+  ])
+
+  return {
+    countdown: state.countdown,
+    isRunning: state.isRunning,
+    start,
+    reset,
+    pause,
+  }
+}
